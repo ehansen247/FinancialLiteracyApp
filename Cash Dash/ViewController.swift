76df216b70345001ca212718d34450425570fa5a
Michael Chen <MichaelChen10@Haiqiangs-MacBook-Air.local>
2018-11-23 23:23:47 -0500

    Initial Commit

---
+++ Cash Dash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cash Dash
+//
+//  Created by Michael Chen on 11/23/18.
+//  Copyright © 2018 Chris50. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
